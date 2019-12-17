51cb051a31b0b221d1b67c08a92d0224b518afbd
Seedockh <pierre.herisse@gmail.com>
2019-12-17 11:37:17 +0100

    Remove useless duets await / then

--- src/controllers/BlobController.ts
+++ src/controllers/BlobController.ts
@@ -4,6 +4,41 @@ import { getEnvFolder } from '../main'
 import Authentifier from '../services/authentifier'
 import Blob from '../entity/Blob'
 
-class BlobController {}
+class BlobController {
+  static retrieveBlob = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
+    return res.status(200).send({ message: 'Init' })
+  }
+
+  static getBlobInfos = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
+    return res.status(200).send({ message: 'Init' })
+  }
+
+  static addBlob = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
+    return res.status(200).send({ message: 'Init' })
+  }
+
+  static duplicateBlob = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
+    return res.status(200).send({ message: 'Init' })
+  }
+
+  static deleteBlob = async (
+    req: Request,
+    res: Response,
+  ): Promise<Response> => {
+    return res.status(200).send({ message: 'Init' })
+  }
+}
 
 export default BlobController
