e333c80214d5d8eeb01a07ae35f1579236e2a0c6
Seedockh <pierre.herisse@gmail.com>
2019-12-17 09:56:23 +0100

    Recreates user folder on login if does not exist

--- src/routes/bucket.ts
+++ src/routes/bucket.ts
@@ -7,6 +7,9 @@ const router = Router()
 // Get all buckets
 router.get('/getAll', [checkJwt], BucketController.getAllBuckets)
 
+// Get to know if a bucket exists
+router.head('/exists/:name([a-z]+)', [checkJwt], BucketController.bucketExists)
+
 // List all files of a bucket
 router.get('/listFiles/:id([0-9]+)', [checkJwt], BucketController.listFiles)
 
