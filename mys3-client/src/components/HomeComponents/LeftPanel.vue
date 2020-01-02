ffd8a870e3ccbcd186c73f589956563312500594
Seedockh <pierre.herisse@gmail.com>
2020-01-02 12:56:27 +0100

    Add creation of bucket and blob

--- mys3-client/src/components/HomeComponents/LeftPanel.vue
+++ mys3-client/src/components/HomeComponents/LeftPanel.vue
@@ -5,12 +5,13 @@
       <li v-on:click="getBuckets">
         <img src="../../assets/folder-icon.png" alt="folder picture">
         {{ this.result.id }}
-        <ul v-if="depth>=1">
-          <li v-on:click="getBlobs" v-for="folder in currentFolders" v-bind:key="folder">
-            <img src="../../assets/folder-icon.png" alt="folder picture">
-            {{ folder }}
-          </li>
-        </ul>
+      </li>
+    </ul>
+    <ul class="buckets-list" v-if="depth>=1">
+      <li v-for="(folder, index) in currentFolders" @click="selected = index" v-on:click="getBlobs" :class="{selected: index === selected}" v-bind:key="folder">
+        <img src="../../assets/folder-icon.png" alt="folder picture">
+        {{ folder }}
+        <img src="../../assets/delete-icon.png" class="delete-icon" alt="folder picture">
       </li>
     </ul>
     <button type="button" name="logout" class="btn-submit" v-on:click="logout">Logout</button>
@@ -25,7 +26,20 @@
     // mixins: [listFolders(0)],
     props: ['result'],
     data() {
-      return { depth: 0, error: undefined, currentFolders: [] }
+      return {
+        depth: 0,
+        error: undefined,
+        currentFolders: [],
+        selected: null,
+      }
+    },
+    mounted() {
+      this.$root.$on('sendDataToLeftPanelComponent', bucket => {
+        if (bucket.new)
+          return this.getBuckets()
+
+        this.getBlobs(bucket)
+      })
     },
     methods: {
       logout() {
@@ -45,33 +59,61 @@
             }
           ).then( result => {
             this.depth++
-            this.$root.$emit('eventing', result.data.list)
+            this.$root.$emit('sendDataToFileListComponent', {
+              list: null,
+              userId: this.result.id,
+              bucketName: null,
+            })
             return this.currentFolders = result.data.list
           }).catch( error => {
+            if (error.response.status === 403)
+              return this.$router.push({ name: 'login' })
             this.error = error.response.data.message
           })
         }
 
-        if (this.depth === 1) {
-          console.log('lower depth')
+        if (this.depth === 1 && this.selected === null) {
           this.depth--
-          this.$root.$emit('eventing', [])
+          this.selected = null
+          this.$root.$emit('sendDataToFileListComponent', {
+            list: null,
+            userId: null,
+            bucketName: null,
+          })
           return this.currentFolders = []
+        }
+
+        if (this.depth === 1 && this.selected >= 0) {
+          this.selected = null
+          return this.$root.$emit('sendDataToFileListComponent', {
+            list: null,
+            userId: this.result.id,
+            bucketName: null,
+          })
         }
       },
 
-      getBlobs() {
+      getBlobs(bucket) {
         this.error = undefined
+        if (typeof bucket === 'object')
+          bucket = bucket.toElement.innerText.trim()
         axios.get(
           // URL
-          'http://localhost:1337/bucket/listFiles/',
+          `http://localhost:1337/bucket/listFiles/${bucket}`,
           // HEADERS
           {
             headers: { 'Authorization': `Bearer ${localStorage.token}` }
           }
         ).then( result => {
-          return this.$root.$emit('eventing', result.data.list)
+          return this.$root.$emit('sendDataToFileListComponent', {
+            list: result.data,
+            userId: this.result.id,
+            bucketName: bucket,
+          })
         }).catch( error => {
+          if (error.response.status === 403)
+            return this.$router.push({ name: 'login' })
+
           this.error = error.response.data.message
         })
       }
@@ -101,19 +143,36 @@ ul {
 }
 
 li {
+  width: 100%;
   margin-left: -2em;
-  margin-bottom: .5em;
+  padding: .3em .5em;
+  border-radius: 3px;
   white-space: pre-line;
   cursor: pointer;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  position: relative;
 }
   li img {
     width: 10px;
+    height: 10px;
     margin-right: .5em
   }
-
-  li ul {
-    margin-top: 1em;
+  .delete-icon {
+    position: absolute;
+    right: 0;
   }
+
+.buckets-list {
+  margin-left: 1em;
+  max-height: 50%;
+  overflow: scroll;
+    -ms-overflow-style: none;
+  }
+.buckets-list::-webkit-scrollbar {
+  display: none;
+}
 
 
 .btn-submit {
@@ -135,10 +194,21 @@ li {
     cursor: pointer;
   }
 
+.selected {
+  background: rgba(50,68,108,1);
+  text-shadow: 2px 2px 3px black;
+}
+  .selected img {
+    box-shadow: 1px 1px 2px black;
+  }
+
 @media screen and (max-width: 640px) {
   #leftpanel-container {
     width: 90%;
-    height: 200px;
+    height: 400px;
+  }
+  .leftpanel-title {
+    margin-top: 1em;
   }
   ul {
     max-height: 100px;
