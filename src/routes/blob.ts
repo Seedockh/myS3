801a004c404c0ad75cb5a2039224f2207f91ce82
Seedockh <pierre.herisse@gmail.com>
2020-01-05 00:31:23 +0100

    Remove unused dependencies

--- src/routes/blob.ts
+++ src/routes/blob.ts
@@ -1,4 +1,4 @@
-import express, { Router } from 'express'
+import { Router } from 'express'
 import BlobController from '../controllers/BlobController'
 import { checkJwt } from '../middlewares/checkJwt'
 
