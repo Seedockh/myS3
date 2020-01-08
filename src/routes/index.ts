1ece1ed823700c8e4077046af17ead6d01074f2e
Seedockh <pierre.herisse@gmail.com>
2020-01-08 11:47:53 +0100

    commit eslit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,12 +10,16 @@ const routes = Router()
 routes.get('/', (req, res) => {
   res.send({ message: 'Welcome on mys3 homepage !' })
 })
-routes.post('/checktoken', (req: Request, res: Response): Response => {
-  const secret:any = process.env.JWT_SECRET
-  const valid = jwt.verify(req.body.token, secret,
-    (err:any) => err ? false : true)
-  return res.send({ valid: valid })
-})
+routes.post(
+  '/checktoken',
+  (req: Request, res: Response): Response => {
+    const secret: any = process.env.JWT_SECRET
+    const valid = jwt.verify(req.body.token, secret, (err: any) =>
+      err ? false : true,
+    )
+    return res.send({ valid: valid })
+  },
+)
 routes.use('/auth', auth)
 routes.use('/user', user)
 routes.use('/bucket', bucket)
