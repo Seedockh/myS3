9a7f913fd380bc662f672510c71fa6bd05f95c89
Seedockh <pierre.herisse@gmail.com>
2020-01-09 14:21:47 +0100

    Rebase with master

--- mys3-client/src/components/HomeComponents/LeftPanel.vue
+++ mys3-client/src/components/HomeComponents/LeftPanel.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="leftpanel-container">
-    <p class="leftpanel-title">{{ this.result.nickname }}</p>
+    <p class="leftpanel-title" @click="updateUser">{{ this.result.nickname }}</p>
     <ul class="userid">
       <li v-on:click="getBuckets">
         <img src="../../assets/folder-icon.png" alt="folder picture">
@@ -50,11 +50,41 @@
       })
     },
     methods: {
+      async updateUser() {
+        swal({
+          title: 'Test modal with input',
+          html: 'custom <strong>content</strong>',
+          input: 'text',
+          inputs: [],
+          preConfirm: (value) => {
+            if (!value) {
+              swal.showValidationError('Should not be empty!')
+            }
+          }
+        }).then( newName => {
+          this.renameBucket(newName)
+          this.editBucket = false
+        })
+        swal.addInput({
+          name: 'ajaxradio',
+          type: 'radio',
+          label: 'Ajax radio input label',
+          options: new Promise((resolve) => {
+            setTimeout(() => {
+              resolve({
+                ajax_foo: 'Ajax Foo',
+                ajax_bar: 'Ajax Bar',
+              })
+            }, 2000)
+          }),
+        })
+      },
+
       handleChangeBucket(folder, index) {
         if (this.selectedIndex === index && this.selectedBucket === folder)
           this.editBucket = true
         else this.editBucket = false
-        
+
         this.selectedIndex = index
         this.selectedBucket = folder
       },
@@ -267,6 +297,7 @@
   margin-top: 0;
   border-radius: 7px 0 0 0;
   text-align: center;
+  cursor: pointer;
 }
 
 ul {
